131057fc08b20d95281779827b5a78db0fc9f171
nichoth <nichoth@gmail.com>
2021-04-17 18:34:43 -0700

    first

---
+++ index.js
@@ -0,0 +1,36 @@
+var fs = require('fs')
+var path = require('path')
+var matter = require('gray-matter')
+var mkdirp = require('mkdirp')
+
+module.exports = {
+    buildThem
+}
+
+function buildThem (inputDir, outputDir, templateFile, makeHs) {
+    fs.readdir(inputDir, function (err, files) {
+        if (err) throw err
+        console.log('files:  ', files)
+
+        files.forEach(fileName => {
+            var _path = path.join(inputDir, fileName)
+            var baseName = path.basename(fileName, '.md')
+
+            fs.readFile(_path, 'utf8', (err, file) => {
+                if (err) throw err
+
+                var outFileDir = outputDir + '/' + baseName
+                mkdirp.sync(outFileDir)
+
+                var hs = makeHs(file, baseName)
+
+                var ws = fs.createWriteStream(outFileDir + '/index.html')
+                var rs = fs.createReadStream(templateFile)
+
+                rs.pipe(hs).pipe(ws)
+            })
+        })
+
+    })
+}
+
